c37269517fa6326704fa4073901cbd4d74c97d25
AboElkhear <ahmed6863817@gmail.com>
2022-08-10 19:05:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,154 @@
+
+// loading to init...........
+
+$(document).ready(function(){
+    $('#ready').fadeOut(2000, function(){
+        $('body').css('overflow','visible');
+        $('#topBtn').css('display' , 'none')
+    })
+})
+
+
+// when click event on topbtn & toggle 
+
+$('#topBtn').click(function(){
+    $('html , body').animate({scrollTop : 0}, 500);
+})
+
+$('#btnTopFooter').click(function(){
+    $('html , body').animate({scrollTop : 0}, 500);
+})
+
+$(window).scroll(function(){
+
+    let whyOffset = $('#why').offset().top;
+    let pricingOffset = $('#pricing').offset().top;
+
+    if($(window).scrollTop() > whyOffset && $(window).scrollTop() < pricingOffset)
+    {
+        $('#topBtn').fadeIn(500);
+    }
+    else
+    {
+        $('#topBtn').fadeOut(250);
+    }
+})
+
+// change toggle navbar style
+
+$('.navbar .navbar-toggler').click(()=>{
+    $('.navbar .navbar-toggler').toggleClass('convert');
+})
+
+
+// change flag for currency when click 
+
+let currency = document.getElementById('currency');
+let img = document.getElementById('img-select');
+currency.addEventListener('click' , function(){
+    if(this.value == 'dollar'){
+        img.removeAttribute('src');
+        img.setAttribute('src' , 'imgs/navbar/Flag_of_the_United_States.svg.png');
+    }
+    else if(this.value == 'pound'){
+        img.removeAttribute('src');
+        img.setAttribute('src' , 'imgs/navbar/Flag_of_Egypt.png');
+    }
+    else if(this.value == 'euro'){
+        img.removeAttribute('src');
+        img.setAttribute('src' , 'imgs/navbar/Flag_of_Europe.svg.png');
+    };
+})
+
+
+// add class active to an active link and smooth move.....
+
+$('.navbar-nav .nav-item a').click(function(){
+    $(this).addClass('active');
+    $(this).parent().siblings().children().removeClass('active');
+    let currentSection = $(this).attr('href');
+    let currentOffset = $(currentSection).offset().top;
+    $('html , body').animate({scrollTop : currentOffset}, 500);
+})
+
+
+// change indicators buttons style
+
+$('#indicators button').click(function(){
+    $(this).addClass('change');
+    $(this).siblings().removeClass('change');
+})
+
+
+// counter section
+
+let counters = document.querySelectorAll('.counter');
+let section = document.querySelector('.numbers')
+
+function startCount(ele){
+    let target = +ele.getAttribute('data-target');
+    let count = setInterval(() =>{
+        ele.textContent++;
+        if(ele.textContent == target){
+            clearInterval(count)
+        }
+    }, 3000/target)
+}
+
+// to start count
+let numbersOffset = $('#numbers').offset().top - 500;
+
+// to stop counter 
+let started = false;
+
+// when scroll to start counter
+
+$(window).scroll(function(){
+    if($(window).scrollTop() >= numbersOffset){
+        if(!started){
+            counters.forEach(counter => startCount(counter))
+        }
+        started = true;
+    }
+})
+
+
+// for email subscribe in footer section
+
+$('#subscribe').click(()=>{
+    if(checkEmail == true){
+        $('#alertSubscribe').html('Successfully Subscribe');
+        $('#alertSubscribe').addClass('text-success');
+        $('#alertSubscribe').removeClass('text-danger');
+    }
+    else {
+        $('#alertSubscribe').html('You should fill it correctly to subscribe');
+        $('#alertSubscribe').addClass('text-danger');
+        $('#alertSubscribe').removeClass('text-success');
+    }
+})
+
+
+let checkEmail;
+function validUserEmailSubscribe(){
+    let regexEmail = /^[a-zA-Z0-9_]{3,15}(@gmail\.com)$/;
+    checkEmail = regexEmail.test($('#emailSubscribe').val());
+    if(checkEmail == true)
+    {
+        $('#emailSubscribe').addClass('is-valid');
+        $('#emailSubscribe').removeClass('is-invalid');
+        $('#alertEmailSubscibe').css('display' , 'none');
+    }
+    else
+    {
+        $('#emailSubscribe').addClass('is-invalid');
+        $('#emailSubscribe').removeClass('is-valid');
+        $('#alertEmailSubscibe').css('display' , 'block');
+    }
+}
+
+$('#emailSubscribe').on('input' , function(){
+    validUserEmailSubscribe();
+})
+
+
---
+++ js/signin.js
@@ -0,0 +1,54 @@
+
+// loading to init...........
+
+$(document).ready(function(){
+    $('#ready').fadeOut(2000, function(){
+        $('body').css('overflow','visible');
+    })
+})
+
+// import list of users from localstorage 
+
+import { users } from "./signup.js";
+
+// ...verification...
+
+let login = document.getElementById('enter-page')
+login.addEventListener('click', loginPage);
+
+function loginPage(){
+    let passwordSignin = document.getElementById('password-signin');
+    let textSignin = document.getElementById('text-signin');
+    let text = textSignin.value;
+    let pass = passwordSignin.value;
+    let alertSignin = document.getElementById('alertSignin');
+
+    if(text == '' || pass == ''){
+        alertSignin.innerHTML = 'All Fields are important..';
+        alertSignin.classList.add('text-danger');
+        alertSignin.classList.remove('text-success');
+        return false;
+    }
+
+    if(users == null && text != '' && pass != ''){
+        alertSignin.innerHTML = 'There is an error in Email, please make a registration first..';
+        alertSignin.classList.add('text-danger');
+        alertSignin.classList.remove('text-success');
+        return false;
+    }
+    
+    else{
+        for(let i = 0; i < users.length; i++){
+            if(users[i].password == pass && (users[i].email == text || users[i].phone == text)){
+                alertSignin.innerHTML = `Hello ${users[i].name}..`;
+                alertSignin.classList.add('text-success');
+                alertSignin.classList.remove('text-danger');
+            }
+            else{
+                alertSignin.innerHTML = 'There is an error in Email, please make a registration first..';
+                alertSignin.classList.add('text-danger');
+                alertSignin.classList.remove('text-success');
+            }
+        }
+    }
+}
